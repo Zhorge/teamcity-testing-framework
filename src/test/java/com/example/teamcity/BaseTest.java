d7b7db340ec904d16bbe230bbb3a4bcde22fd7d7
Zhorge <Andrei.zhornick@yandex.ru>
2025-02-10 00:11:50 +0300

    Add config, spec, base tests

---
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -0,0 +1,5 @@
+package com.example.teamcity;
+
+public class BaseTest {
+
+}
---
+++ src/test/java/com/example/teamcity/api/DummyTest.java
@@ -0,0 +1,22 @@
+package com.example.teamcity.api;
+
+import com.example.teamcity.BaseTest;
+import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.spec.Specifications;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class DummyTest extends BaseTest {
+
+  @Test
+  public void userShouldBeAbleGetAllProjects() {
+    RestAssured
+        .given()
+        .spec(Specifications.getSpec()
+                .authSpec(
+                    User.builder().user("admin").password("admin").build()
+                )
+        )
+        .get("/app/rest/projects");
+  }
+}
