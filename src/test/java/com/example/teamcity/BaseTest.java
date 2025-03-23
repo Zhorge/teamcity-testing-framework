dae8d30242e35ebaddaa9dd136d700ddbc580ae4
Zhorge <Andrei.zhornick@yandex.ru>
2025-03-23 23:29:53 +0300

    feat(api): automate perProjectPermissions setup and restore

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -1,19 +1,13 @@
 package com.example.teamcity;
 
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
-import static org.apache.http.HttpStatus.SC_OK;
 
-import com.example.teamcity.api.enums.Endpoint;
 import com.example.teamcity.api.generators.TestDataStorage;
-import com.example.teamcity.api.models.settings.AuthSettings;
 import com.example.teamcity.api.models.TestData;
 import com.example.teamcity.api.requests.CheckedRequests;
-import com.example.teamcity.api.requests.unchecked.UncheckedBase;
 import com.example.teamcity.api.spec.Specifications;
-import io.qameta.allure.Step;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.asserts.SoftAssert;
 
 public class BaseTest {
@@ -21,11 +15,6 @@ public class BaseTest {
   protected CheckedRequests superUserCheckRequests = new CheckedRequests(Specifications.superUserSpec());
   protected CheckedRequests userCheckedRequests;
   protected TestData testData;
-
-  @BeforeSuite(alwaysRun = true)
-  public void beforeAll() {
-    updateAuthSettings();
-  }
 
   @BeforeMethod(alwaysRun = true)
   public void beforeTest() {
@@ -38,29 +27,5 @@ public class BaseTest {
   public void afterTest() {
     TestDataStorage.getStorage().deleteCreatedEntities();
     softy.assertAll();
-  }
-
-  @Step("Enable per-project permissions via API")
-  private void updateAuthSettings() {
-    // 1. Получаем текущие настройки
-    AuthSettings currentSettings =
-        new UncheckedBase(Specifications.superUserSpec(), Endpoint.SERVER_AUTH_SETTINGS)
-            .read()
-            .as(AuthSettings.class);
-
-// 2. Обновляем нужные параметры
-    AuthSettings updatedSettings = AuthSettings.builder()
-        .perProjectPermissions(true)
-        .modules(currentSettings.getModules())
-        .allowGuest(currentSettings.getAllowGuest())
-        .emailVerification(currentSettings.getEmailVerification())
-        .guestUsername(currentSettings.getGuestUsername())
-        .build();
-
-    // 3. Отправляем обновленные настройки
-    new UncheckedBase(Specifications.superUserSpec(), Endpoint.SERVER_AUTH_SETTINGS)
-        .update(updatedSettings)
-        .then()
-        .statusCode(SC_OK);
   }
 }
--- src/test/java/com/example/teamcity/api/BaseApiTest.java
+++ src/test/java/com/example/teamcity/api/BaseApiTest.java
@@ -1,7 +1,60 @@
 package com.example.teamcity.api;
 
+import static org.apache.http.HttpStatus.SC_OK;
+
 import com.example.teamcity.BaseTest;
+import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.models.settings.AuthSettings;
+import com.example.teamcity.api.requests.unchecked.UncheckedBase;
+import com.example.teamcity.api.spec.Specifications;
+import io.qameta.allure.Step;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class BaseApiTest extends BaseTest {
+  private final UncheckedBase serverAuthRequest =
+      new UncheckedBase(Specifications.superUserSpec(), Endpoint.SERVER_AUTH_SETTINGS);
+  private AuthSettings currentSettings;
 
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() {
+    setUpServerAuthSettings();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void afterAll() {
+    cleanUpServerAuthSettings();
+  }
+
+  @Step("Enable per-project permissions via API")
+  private void setUpServerAuthSettings() {
+    // 1. Получаем текущие настройки
+    currentSettings = serverAuthRequest.read().as(AuthSettings.class);
+
+    // 2. Обновляем нужные параметры
+    AuthSettings updatedSettings =
+        AuthSettings.builder()
+            .perProjectPermissions(true)
+            .modules(currentSettings.getModules())
+            .allowGuest(currentSettings.getAllowGuest())
+            .emailVerification(currentSettings.getEmailVerification())
+            .guestUsername(currentSettings.getGuestUsername())
+            .build();
+
+    // 3. Отправляем обновленные настройки
+    serverAuthRequest.update(updatedSettings).then().statusCode(SC_OK);
+  }
+
+  @Step("Return per-project permissions")
+  private void cleanUpServerAuthSettings() {
+    // Возвращаем настройке perProjectPermissions исходное значение
+    serverAuthRequest.update(
+        AuthSettings.builder()
+            .perProjectPermissions(currentSettings.getPerProjectPermissions())
+            .modules(currentSettings.getModules())
+            .allowGuest(currentSettings.getAllowGuest())
+            .emailVerification(currentSettings.getEmailVerification())
+            .guestUsername(currentSettings.getGuestUsername())
+            .build());
+  }
 }
